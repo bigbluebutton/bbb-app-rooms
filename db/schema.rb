48e0aae0dcf2c948a206184afad8d4da3c30851b
jfederico <jesus@123it.ca>
2020-06-19 11:08:53 -0400

    LTI-32: Removed support for sqlite and fixed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_28_195723) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.string "description"
