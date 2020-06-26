27f77503844477feee9a86844a66174e3ee8e073
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-06-25 21:42:31 -0300

    Continue the implementation of the theme for Elos

---
+++ themes/elos/helpers/elos_helper.rb
@@ -0,0 +1,30 @@
+module ElosHelper
+
+  def format_date(date, format=:short_custom, include_time=true)
+    if date.present?
+      if date.is_a?(Integer) && date.to_s.length == 13
+        value = Time.at(date/1000)
+      else
+        value = Time.at(date)
+      end
+      if include_time
+        I18n.l(value, format: format)
+      else
+        I18n.l(value.to_date, format: format)
+      end
+    else
+      nil
+    end
+  end
+
+  def recording_duration_secs(recording)
+    playbacks = recording[:playbacks]
+    valid_playbacks = playbacks.reject { |p| p[:type] == 'statistics' }
+    len = valid_playbacks.first[:length]
+    if len.nil?
+      0
+    else
+      len * 60
+    end
+  end
+end
