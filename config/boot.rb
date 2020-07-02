5306d0bdd1c3c00806d52e4263be41e4ff6d982b
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-07-02 19:55:36 -0300

    Setup bootsnap only for development

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+if Rails.env.development?
+  require "bootsnap/setup"
+end
