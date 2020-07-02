bf821c007a0d3493e99f3f2a3d39a1d87e760e5d
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-07-02 20:09:52 -0300

    Enable bootsnap again in prod, call preload_app! for puma

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-if Rails.env.development?
-  require "bootsnap/setup"
-end
+require "bootsnap/setup"
