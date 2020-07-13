d536844d56c7e30b727c816ad7a26a414b38c3d1
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-07-13 09:08:23 -0300

    Add env var ASSET_HOST to configure an asset host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  unless ENV['ASSET_HOST'].blank?
+    config.action_controller.asset_host = ENV['ASSET_HOST']
+  end
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
