f08d17ad4a8c406ab11c3a448cf88553fd010b00
Mariam05 <tomariam@gmail.com>
2020-08-26 20:22:53 +0000

    added stub for api calls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,16 @@ RSpec.configure do |config|
 
   config.before(:each) do
     DatabaseCleaner.strategy = :truncation
+    stub_request(:any, /#{"\/bigbluebutton\/api"}/)
+      .with(
+        headers:
+        {
+          'Accept': '*/*',
+          'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent': 'Ruby',
+        }
+      )
+      .to_return(status: 200, body: '', headers: {})
   end
 
   config.after(:each) do
