9733a864f573a126a6909c1329b1b5de470e9c3a
Mariam A <tomariam@gmail.com>
2020-08-28 12:29:13 -0400

    LTI-46: Show to Admins and Teachers a button for ending meeting when it is running (#55)
    * LTI-46: Added end meeting button
    
    * added stub for api calls
    
    * LTI-46: Fixed button not showing on join
    
    * LTI-46: Remove button when meeting is ended from bbb
    
    * Update meetingInfo.js
    
    Co-authored-by: Jesus Federico <jesus@123it.ca>

---
+++ app/assets/javascripts/meetingInfo.js
@@ -0,0 +1,28 @@
+$(document).on('turbolinks:load', function(){
+  var room = window.location.pathname.split('/')[3];
+  App.meetingInfo = App.cable.subscriptions.create({channel: "MeetingInfoChannel", room_id: room}, {
+    connected: function() {
+        console.log("Connected to meeting info channel");
+    },
+    disconnected: function() {
+  
+    },
+    received: function(data) {
+      console.log("received data: " + JSON.stringify(data));
+      if (data.meeting_in_progress == true){
+        show_end_meeting_btn();
+      }
+      if (data.action == "end"){
+        hide_end_meeting_btn();          
+      }
+    }
+  });
+});
+
+var show_end_meeting_btn = function(){
+  $('#end-meeting-btn').show();
+}
+
+var hide_end_meeting_btn = function(){
+  $('#end-meeting-btn').hide();
+}
