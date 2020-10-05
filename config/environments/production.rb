997caa7c09227de9a79187791784ae074ab49b54
Jesus Federico <jesus@123it.ca>
2020-10-05 17:07:22 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
                                                                  config.logger.warn("Support: Redis cache action #{method} failed and returned '#{returning}': #{exception}")
                                                                }, },]
                        else
-                         config.cache_store = :file_store, Rails.root.join('tmp/cache_store')
+                         :memory_store
                        end
 
   config.hosts = ENV['WHITELIST_HOST'].presence || nil
