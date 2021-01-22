f046f42d9bbd6f841145069beb889b7c4aa0e31b
Mariam A <tomariam@gmail.com>
2021-01-22 17:12:24 -0500

    Some performance improvements (#81)
    * Some performance improvements
    
    * Update Gemfile.lock
    
    Co-authored-by: Jesus Federico <jesus@123it.ca>

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,26 @@
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
-    ]
+// postcss.config.js
+let environment = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+};
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/assets/stylesheets/*.scss'
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
+
+module.exports = environment;
