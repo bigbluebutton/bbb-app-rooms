cde0cdce51b0c4880407c902a92de9600d1377fd
Christopher Pottes <christopher@mconf.com>
2021-06-16 14:28:54 -0300

    Addded folders theme RNP

---
+++ themes/rnp/assets/javascripts/theme-application-rnp.js
@@ -0,0 +1,60 @@
+//= require flatpickr/dist/flatpickr
+//= require clipboard
+
+$(document).on('turbolinks:load', function(){
+  $('.toast').toast();
+  $(".toast.toast-auto").each(function() {
+    $(this).toast('show');
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $(".datepicker").each(function() {
+    var format = $(this).data('format');
+    $(this).flatpickr({
+      disableMobile: true,
+      enableTime: false,
+      dateFormat: format,
+      minDate: new Date(),
+    });
+  });
+
+  $(".timepicker").each(function() {
+    var format = $(this).data('format');
+    $(this).flatpickr({
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: format,
+      time_24hr: true,
+    });
+  });
+
+  $(".timepicker-duration").each(function() {
+    var format = $(this).data('format');
+    $(this).flatpickr({
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: format,
+      time_24hr: true,
+      minuteIncrement: 1,
+      minTime: "00:10",
+    });
+  });
+
+  $(".copy-to-clipboard").each(function() {
+    $toast = $('.toast', $(this).data('toast-id'));
+    clipboard = new ClipboardJS(this);
+    clipboard.on('success', function(e) {
+      $toast.toast('dispose');
+      $toast.toast('show');
+    });
+  });
+
+  $(".btn-retry").on('click', function() {
+    window.open($(this).data('launch'));
+    $(this).addClass('disabled');
+    $(this).attr('disabled', '1');
+    $(this).removeData('launch');
+    return true;
+  });
+});
---
+++ themes/rnp/helpers/elos_helper.rb
@@ -0,0 +1,73 @@
+module ElosHelper
+
+  def format_date(date, format=:short_custom, include_time=true)
+    if date.present?
+      if date.is_a?(Integer) && date.to_s.length == 13
+        value = Time.zone.at(date/1000)
+      else
+        value = Time.zone.at(date)
+      end
+      if include_time
+        I18n.l(value, format: format)
+      else
+        I18n.l(value.to_date, format: format)
+      end
+    else
+      nil
+    end
+  end
+
+  def recording_duration_secs(recording)
+    playbacks = recording[:playbacks]
+    valid_playbacks = playbacks.reject { |p| p[:type] == 'statistics' }
+    return 0 if valid_playbacks.empty?
+
+    len = valid_playbacks.first[:length]
+    return 0 if len.nil?
+
+    len * 60
+  end
+
+  def duration_in_hours_and_minutes(duration)
+    distance_of_time_in_hours_and_minutes(0, duration)
+  end
+
+  def distance_of_time_in_hours_and_minutes(from_time, to_time)
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+    distance_in_hours   = (((to_time - from_time).abs) / 3600).floor
+    distance_in_minutes = ((((to_time - from_time).abs) % 3600) / 60).round
+
+    words = ''
+
+    if distance_in_hours > 0
+      words << I18n.t('helpers.distance_of_time_in_hours_and_minutes.hour', count: distance_in_hours)
+      if distance_in_minutes > 0
+        words << " #{I18n.t('helpers.distance_of_time_in_hours_and_minutes.connector')} "
+      end
+    end
+
+    if distance_in_minutes > 0
+      words << I18n.t('helpers.distance_of_time_in_hours_and_minutes.minute', count: distance_in_minutes)
+    end
+
+    words
+  end
+
+  def current_formatted_time_zone
+    ActiveSupport::TimeZone[Time.zone.name].to_s.gsub(/[^\s]*\//, '').gsub(/_/, ' ')
+  end
+
+  def get_custom_duration(duration)
+    duration_in_time = ScheduledMeeting.convert_duration_to_time(duration)
+    return time = duration_in_time[0].to_s + ':' + duration_in_time[1].to_s
+  end
+
+  def playback_url(room, record_id, playback)
+    if Rails.application.config.playback_url_authentication
+      recording_playback_url(room, record_id, playback[:type])
+    else
+      playback[:url]
+    end
+  end
+end
