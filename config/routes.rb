c067dd9cb26aa10f44ea20a576c6c009e89cd4d2
Christopher Pottes <christopher@mconf.com>
2021-07-01 14:42:38 -0300

    Fix configs to rails admin and added validation for show rails admin or application in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
+  if ENV['SERVE_RAILS_ADMIN'] == true.to_s
     mount RailsAdmin::Engine => '/dash', as: 'rails_admin'
   end
 
@@ -9,62 +9,64 @@ Rails.application.routes.draw do
     mount ActionCable.server => Rails.configuration.action_cable.mount_path
   end
 
-  scope ENV['RELATIVE_URL_ROOT'] || '' do
-    scope 'rooms' do
-      get '/health_check', to: 'health_check#all', default: { format: nil }
-      get '/healthz', to: 'health_check#all', default: { format: nil }
+  if ENV['SERVE_APPLICATION'] == true.to_s
+    scope ENV['RELATIVE_URL_ROOT'] || '' do
+      scope 'rooms' do
+        get '/health_check', to: 'health_check#all', default: { format: nil }
+        get '/healthz', to: 'health_check#all', default: { format: nil }
 
-      get '/close', to: 'rooms#close', as: :autoclose
+        get '/close', to: 'rooms#close', as: :autoclose
 
-      # Handles recording management.
-      scope ':id/recording/:record_id' do
-        get '/playback/:playback_type', to: 'rooms#recording_playback', as: :recording_playback
-        post '/publish', to: 'rooms#recording_publish', as: :recording_publish
-        post '/unpublish', to: 'rooms#recording_unpublish', as: :recording_unpublish
-        post '/protect', to: 'rooms#recording_protect', as: :recording_protect
-        post '/unprotect', to: 'rooms#recording_unprotect', as: :recording_unprotect
-        post '/update', to: 'rooms#recording_update', as: :recording_update
-        post '/delete', to: 'rooms#recording_delete', as: :recording_delete
+        # Handles recording management.
+        scope ':id/recording/:record_id' do
+          get '/playback/:playback_type', to: 'rooms#recording_playback', as: :recording_playback
+          post '/publish', to: 'rooms#recording_publish', as: :recording_publish
+          post '/unpublish', to: 'rooms#recording_unpublish', as: :recording_unpublish
+          post '/protect', to: 'rooms#recording_protect', as: :recording_protect
+          post '/unprotect', to: 'rooms#recording_unprotect', as: :recording_unprotect
+          post '/update', to: 'rooms#recording_update', as: :recording_update
+          post '/delete', to: 'rooms#recording_delete', as: :recording_delete
+        end
+
+        # Handles launches.
+        get '/launch', to: 'rooms#launch', as: :room_launch
+
+        # Handles sessions.
+        get '/sessions/create'
+        get '/sessions/failure'
+
+        # Handles Omniauth authentication.
+        get '/auth/:provider', to: 'sessions#new', as: :omniauth_authorize
+        get '/auth/:provider/callback', to: 'sessions#create', as: :omniauth_callback
+        get '/auth/:provider/failure', to: 'sessions#failure', as: :omniauth_failure
+        get '/auth/:provider/retry', to: 'sessions#retry', as: :omniauth_retry
+
+        # Handles errors.
+        get '/errors/:code', to: 'errors#index', as: :errors
       end
 
-      # Handles launches.
-      get '/launch', to: 'rooms#launch', as: :room_launch
-
-      # Handles sessions.
-      get '/sessions/create'
-      get '/sessions/failure'
-
-      # Handles Omniauth authentication.
-      get '/auth/:provider', to: 'sessions#new', as: :omniauth_authorize
-      get '/auth/:provider/callback', to: 'sessions#create', as: :omniauth_callback
-      get '/auth/:provider/failure', to: 'sessions#failure', as: :omniauth_failure
-      get '/auth/:provider/retry', to: 'sessions#retry', as: :omniauth_retry
-
-      # Handles errors.
-      get '/errors/:code', to: 'errors#index', as: :errors
-    end
-
-    # NOTE: there are other actions in the rooms controller, but they are not used for now,
-    #       rooms are automatically created when needed and can't be edited
-    resources :rooms, only: :show do
-      member do
-        get :recordings
-        get '/error/:code', to: 'rooms#error'
-      end
-
-      resources :reports, only: :index do
-        get :download, on: :collection
-      end
-
-      resources :scheduled_meetings, only: [:new, :create, :edit, :update, :destroy] do
+      # NOTE: there are other actions in the rooms controller, but they are not used for now,
+      #       rooms are automatically created when needed and can't be edited
+      resources :rooms, only: :show do
         member do
-          post :join
-          get :external
-          get :wait
-          get :running
-          get :send_create_calendar_event, to: 'brightspace#send_create_calendar_event'
-          get :send_update_calendar_event, to: 'brightspace#send_update_calendar_event'
-          get :send_delete_calendar_event, to: 'brightspace#send_delete_calendar_event'
+          get :recordings
+          get '/error/:code', to: 'rooms#error'
+        end
+
+        resources :reports, only: :index do
+          get :download, on: :collection
+        end
+
+        resources :scheduled_meetings, only: [:new, :create, :edit, :update, :destroy] do
+          member do
+            post :join
+            get :external
+            get :wait
+            get :running
+            get :send_create_calendar_event, to: 'brightspace#send_create_calendar_event'
+            get :send_update_calendar_event, to: 'brightspace#send_update_calendar_event'
+            get :send_delete_calendar_event, to: 'brightspace#send_delete_calendar_event'
+          end
         end
       end
     end
