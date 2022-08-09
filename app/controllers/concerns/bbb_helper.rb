3157df7e14ae4692ee2ba929e141b63b2d3eb6b6
jfederico <jesus@123it.ca>
2022-08-09 17:27:14 -0400

    LTI-146: update BBB gem to 1.9

--- app/controllers/concerns/bbb_helper.rb
+++ app/controllers/concerns/bbb_helper.rb
@@ -24,7 +24,8 @@ module BbbHelper
     @bbb_credentials ||= initialize_bbb_credentials
     bbb_url = remove_slash(@bbb_credentials.endpoint(@room.tenant))
     bbb_secret = @bbb_credentials.secret(@room.tenant)
-    BigBlueButton::BigBlueButtonApi.new(bbb_url, bbb_secret, '0.9', (Rails.configuration.log_level.to_s == 'debug'))
+    bbb_sha256 = true
+    BigBlueButton::BigBlueButtonApi.new(bbb_url, bbb_secret, '1.0', Rails.logger, bbb_sha256)
   end
 
   # Generates URL for joining the current @room meeting.
