3c43318b4185b4386781152c6eeb21211c37ac02
Jesus Federico <jesus@123it.ca>
2022-08-12 09:18:54 -0400

    LTI-148: follow-up fix for lograge (#129)
    * LTI-148: follow-up fix for lograge
    
    * removed references to RAILS_LOG_TO_STDOUT
    
    * fix rubocops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,20 +98,18 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV['RAILS_LOG_TO_STDOUT'] == 'true'
-    # Disable output buffering when STDOUT isn't a tty (e.g. Docker images, systemd services)
-    STDOUT.sync = true
-    logger = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  elsif ENV['RAILS_LOG_REMOTE_NAME'] && ENV['RAILS_LOG_REMOTE_PORT']
+  # Disable output buffering when STDOUT isn't a tty (e.g. Docker images, systemd services)
+  STDOUT.sync = true
+  logger = ActiveSupport::Logger.new(STDOUT)
+
+  if ENV['RAILS_LOG_REMOTE_NAME'] && ENV['RAILS_LOG_REMOTE_PORT']
     require 'remote_syslog_logger'
     logger_program = ENV['RAILS_LOG_REMOTE_TAG'] || "bbb-lti-broker-#{ENV['RAILS_ENV']}"
     logger = RemoteSyslogLogger.new(ENV['RAILS_LOG_REMOTE_NAME'],
                                     ENV['RAILS_LOG_REMOTE_PORT'], program: logger_program)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
 
   # configure redis for ActionCable
   config.cache_store = if ENV['REDIS_URL'].present?
