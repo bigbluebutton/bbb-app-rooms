5d6d38b73b4738a3763e5b46de236a0221feea34
Amir Khemissi <amir.khemissi@insat.ucar.tn>
2023-03-07 11:44:42 +0000

    Deployment: Updated `scripts/start.sh`.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,45 @@
 #!/bin/sh
 
+
+PORT="${PORT:=3000}"
+
+echo ">>> LTI broker starting on port: $PORT"
+
+if [ "$RAILS_ENV" = "production" ]; then
+  # Parse Rails DATABASE and REDIS urls to get host and port.
+  TXADDR=${DATABASE_URL/*:\/\/}
+  TXADDR=${TXADDR/*@/}
+  TXADDR=${TXADDR/\/*/}
+  IFS=:; set - $TXADDR; IFS=' '
+  PGHOST=${1}
+  PGPORT=${2:-5432}
+
+  echo ">>> Connecting to Postgres on $PGHOST:$PGPORT"
+  while ! nc -zw3 $PGHOST $PGPORT 2> /dev/null 1>&2
+  do
+    echo -n '.'
+    sleep 1
+  done
+  echo
+  echo "Connected to Postgres!"
+
+  TXADDR=${REDIS_URL/*:\/\/}
+  TXADDR=${TXADDR/*@/}
+  TXADDR=${TXADDR/\/*/}
+  IFS=:; set - $TXADDR; IFS=' '
+  RDHOST=${1}
+  RDPORT=${2:-6379}
+
+  echo ">>> Connecting to Redis on $RDHOST:$RDPORT"
+  while ! nc -zw3 $RDHOST $RDPORT 2> /dev/null 1>&2
+  do
+    echo -n '.'
+    sleep 1
+  done
+  echo
+  echo "Connected to Redis!"
+fi
+
 db_create=$(RAILS_ENV=$RAILS_ENV bundle exec rake db:create)
 echo $db_create
 
@@ -9,14 +49,7 @@ if [ "$db_create" = "${db_create%"already exists"*}" ]; then
 else
   echo ">>> Database initialization"
   bundle exec rake db:schema:load
-  bundle exec rake db:seed
 fi
 
-# Assets are precompiled on start because the root can change based on ENV["RELATIVE_URL_ROOT"]
-if [ "x${RELATIVE_URL_ROOT}" != "x" ]; then
-  echo ">>> Precompile assets..."
-  bundle exec rake assets:precompile --trace
-fi
-
-echo ">>> Start app..."
-bundle exec rails s -b 0.0.0.0 -p 3000
+echo "Start app..."
+bundle exec rails s -b 0.0.0.0 -p $PORT
