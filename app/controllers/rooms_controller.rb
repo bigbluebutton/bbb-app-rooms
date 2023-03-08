f4ef278646202ad7249ca4d4d4039fdb5b756a0c
jfederico <jesus@123it.ca>
2023-03-08 18:17:37 -0500

    LTI-208d: refactorng on new rooms for legacy launches

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -260,28 +260,33 @@ class RoomsController < ApplicationController
     set_error('forbidden', :forbidden) && return unless launch_params['user_id'] == session[@launch_nonce]['uid']
 
     # Continue through happy path.
-    launch_room(launch_params, session_params['tenant'])
+    launch_room(launch_params, session_params['tenant'].presence)
     launch_user(launch_params) if @room
   end
 
   def launch_room(launch_params, tenant)
     handler = Digest::SHA1.hexdigest("rooms#{tenant}#{launch_params['resource_link_id']}")
-    @room = Room.find_by(handler: handler, handler_legacy: nil, tenant: (tenant.empty? ? nil : tenant))
-    return if @room # this is a regular launch on an existing room.
+    handler_legacy = launch_params['custom_params']['custom_handler_legacy'].presence
+    @room = Room.find_by(handler: handler, handler_legacy: handler_legacy, tenant: tenant)
 
-    handler_legacy = launch_params['custom_params'].key?('custom_handler_legacy') ? launch_params['custom_params']['custom_handler_legacy'] : nil
-    @room = Room.find_by(handler_legacy: handler_legacy, tenant: (tenant.empty? ? nil : tenant))
-    return if @room # this is a legacy launch on existing room already migrated.
+    # Exit if this is a launch on an existing room or continue path for new rooms.
+    return if @room
 
+    # For a legacy launch.
+    unless handler_legacy.nil?
+      # Fetch room parameters if the API is enabled.
+      fetched_room_params = fetch_new_room_params(handler, handler_legacy) if Rails.configuration.handler_legacy_api_enabled
+      # Create new room with fetched params if fetched.
+      @room = Room.create(fetched_room_params.merge({ tenant: tenant })) if fetched_room_params
+      # Exit if the room was created.
+      return if @room
+      # When new room creation is disabled, there is nothing else to do, just exit.
+      return unless Rails.configuration.handler_legacy_new_room_enabled
+    end
+
+    # For any regular launch or if the new room creation for legacy launches is enabled.
     launch_room_params = launch_params_to_new_room_params(handler, handler_legacy, launch_params)
-    @room = Room.create(launch_room_params) && return unless handler_legacy # this is a regular launch on an new room.
-
-    # this is a legacy launch on a new room
-    fetched_room_params = fetch_new_room_params(handler, handler_legacy) if Rails.configuration.handler_legacy_api_enabled
-
-    @room = Room.create(fetched_room_params) && return if fetched_room_params # create new room with fetched params if fetched.
-
-    @room = Room.create(launch_room_params) if Rails.configuration.handler_legacy_new_room_enabled # create new room with launch params only if allowed.
+    @room = Room.create(launch_room_params.merge({ tenant: tenant }))
   end
 
   def launch_user(launch_params)
