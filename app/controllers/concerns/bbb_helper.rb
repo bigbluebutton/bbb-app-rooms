c9490d4e1d07f39f8e4ba626027eeda6e2b34ba2
jfederico <jesus@123it.ca>
2024-05-09 10:41:45 -0400

    [LTI-369] quick refactoring to the way settings are fetched from broker

--- app/controllers/concerns/bbb_helper.rb
+++ app/controllers/concerns/bbb_helper.rb
@@ -196,7 +196,7 @@ module BbbHelper
   end
 
   def bigbluebutton_moderator_roles
-    roles_params = bbb_moderator_roles_params(@room.tenant)
+    roles_params = tenant_setting(@room.tenant, 'bigbluebutton_moderator_roles')&.split(',')
     roles_params.presence || Rails.configuration.bigbluebutton_moderator_roles.split(',')
   end
 
